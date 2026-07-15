12f12147baf74a725194a314b10511f0bb15b205
Gus012903 <153580773+Gus012903@users.noreply.github.com>
2026-07-14 19:00:40 -0500

    Update API URL to monoschinos.st

--- src/api.ts
+++ src/api.ts
@@ -7,7 +7,7 @@ export const headers = {
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
 }
 
-export const url = 'https://monoschinos2.com'
+export const url = 'https://monoschinos.st'
 
 export const api: API = {
   home: `${url}`,
